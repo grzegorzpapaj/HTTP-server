2c353eee3a87b52672872558f42ab1eaa482cae5
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2024-03-21 12:24:41 +0100

    Support writing to file

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class RequestHandler {
-
-    private final String RESPONSE_400_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
     private final String RESPONSE_200_OK = "HTTP/1.1 200 OK\r\n";
+    private final String RESPONSE_201_CREATED = "HTTP/1.1 201 CREATED\r\n";
+    private final String RESPONSE_400_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
+    private final String RESPONSE_507_INSUFFICIENT_STORAGE = "HTTP/1.1 507 Insufficient Storage\r\n";
     private final String ENDL = "\r\n";
 
-    public String readRequestFromBufferedReader(BufferedReader reader) throws IOException {
+    public String readRequestFromBufferedReader(BufferedReader bufferedReader) throws IOException {
 
         StringBuilder requestBuilder = new StringBuilder();
         String line;
 
-        while((line = reader.readLine()) != null && !line.isEmpty()) {
+        while((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
             requestBuilder.append(line).append("\r\n");
+        }
+
+        requestBuilder.append("\r\n");
+
+        while(bufferedReader.ready()) {
+            requestBuilder.append((char) bufferedReader.read());
         }
 
         return requestBuilder.toString();
@@ -25,6 +33,8 @@ public class RequestHandler {
     public String executeRequest(String requestType, String request, String directory) throws IOException {
         if(requestType.equals("GET")) {
             return getResponse(request, directory);
+        } else if(requestType.equals("POST")) {
+            return postResponse(request, directory);
         }
 
         return RESPONSE_400_NOT_FOUND + ENDL;
@@ -67,6 +77,33 @@ public class RequestHandler {
         else {
             return RESPONSE_400_NOT_FOUND + ENDL;
         }
+    }
+
+    private String postResponse(String request, String directory) throws IOException {
+        String path = extractPathFromRequest(request);
+
+        if(path.matches("/files/(.*)")) {
+            String filename = path.substring(7);
+            String content = request.split("\r\n\r\n")[1];
+
+            try {
+                Path directoryPath = Paths.get(directory);
+                Path filePath = directoryPath.resolve(filename);
+
+                Files.createDirectories(directoryPath);
+                Files.createFile(filePath);
+
+                Files.write(filePath, content.getBytes());
+
+                return RESPONSE_201_CREATED + ENDL;
+
+            } catch (FileAlreadyExistsException e) {
+
+                return RESPONSE_507_INSUFFICIENT_STORAGE + ENDL;
+            }
+        }
+
+        return RESPONSE_400_NOT_FOUND;
     }
 
     private String getResponseStringWithContentIf200(String content, String contentType) {
